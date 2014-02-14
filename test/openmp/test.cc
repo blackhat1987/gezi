5a07825a7c2a577e98241a0650c48b15f9b23a88
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-02-14 14:59:22 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@6348 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ test/openmp/test.cc
@@ -0,0 +1,42 @@
+/**
+*  ==============================================================================
+*
+*          \file   .\openmp\test.cc
+*
+*        \author   gezi
+*
+*          \date   2014-02-13 11:58:47.794000
+*
+*  \Description:
+*
+*  ==============================================================================
+*/
+
+#define private public
+#define protected public
+#include "common_util.h"
+
+using namespace std;
+using namespace gezi;
+
+DEFINE_string(type, "simple", "");
+
+TEST(, func)
+{
+#pragma omp parallel
+	{
+		std::cout << "Hello World!\n";
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	FLAGS_logtostderr = true;
+	testing::InitGoogleTest(&argc, argv);
+	google::InitGoogleLogging(argv[0]);
+	google::InstallFailureSignalHandler();
+	int s = google::ParseCommandLineFlags(&argc, &argv, false);
+	boost::progress_timer timer;
+
+	return RUN_ALL_TESTS();
+}
