606e1f805651ab860086d4e8164b844fb0678c41
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-03-02 07:07:36 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@6552 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ python-wrapper/test-seg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding=gbk
+# ==============================================================================
+#          \file   test-seg.py
+#        \author   chenghuige  
+#          \date   2014-03-01 13:32:30.259515
+#   \Description  
+# ==============================================================================
+
+import sys,os
+from libgezi import *
+
+LogHelper.set_level(4)
+
+seg_init('./data/wordseg/', 0, './conf/scw.conf')
+
+s = 'a我爱你中华中华人民共和国第一检察院院长长春市长春药店读美文学作文'
+print s
+
+s2 = segment(s)
+
+print s2
+
+s = '速去欧阳丹丹吧拿吧'
+print segment(s)
+
+print segment('速去郭美美吧拿')
