1331c1df08610a88229494f4d9dc28dd038fb8cb
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-03-30 05:37:38 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@7027 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ test/test_random.cc
@@ -0,0 +1,43 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_random.cc
+ *
+ *        \author   chenghuige   
+ *
+ *          \date   2014-03-30 13:37:29.869194
+ *  
+ *  \Description:
+ *
+ *  ==============================================================================
+ */
+
+#define private public
+#define protected public
+#include "common_util.h"
+
+using namespace std;
+using namespace gezi;
+DEFINE_int32(level, 0, "min log level");
+DEFINE_string(i, "", "input");
+DEFINE_string(o, "", "output");
+DEFINE_string(type, "simple", "");
+
+TEST(test_random, func)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  int s = google::ParseCommandLineFlags(&argc, &argv, false);
+  if (FLAGS_log_dir.empty())
+    FLAGS_logtostderr = true;
+  FLAGS_minloglevel = FLAGS_level;
+  boost::progress_timer timer;
+  
+  return RUN_ALL_TESTS();
+}
