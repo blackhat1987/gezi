067430ea3b52bf3354071ac16c5d028a6e2e7cbd
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-03-30 05:12:27 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@7026 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/random_util.h
@@ -0,0 +1,21 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   random_util.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-03-30 13:12:25.981986
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef RANDOM_UTIL_H_
+#define RANDOM_UTIL_H_
+
+namespace gezi {
+
+}  //----end of namespace gezi
+
+#endif  //----end of RANDOM_UTIL_H_
