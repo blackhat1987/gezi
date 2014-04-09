18e133a0b2d4203a85601bbfc53dce17aa9657dc
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-09 02:16:39 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@7317 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/Numeric/Vector/vector_util.h
@@ -0,0 +1,116 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   Numeric/Vector/vector_util.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-04-09 08:37:54.558044
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef NUMERIC__VECTOR_VECTOR_UTIL_H_
+#define NUMERIC__VECTOR_VECTOR_UTIL_H_
+#include "common_def.h"
+#include "Numeric/Vector/Vector.h"
+namespace gezi {
+
+	//dense vector and binary vec apply
+	template<typename Func>
+	void apply(Fvec& values, const ivec& shiftIndices, Func func)
+	{
+		int bI = 0;
+		for (size_t i = 0; i < values.size(); i++)
+		{
+			Float bVal = 0;
+			if (bI < shiftIndices.size() && i == shiftIndices[bI])
+			{
+				bI++;
+				bVal = 1;
+			}
+			values[i] = func(i, values[i], bVal);
+		}
+	}
+
+	//@TODO 也许需要改为std::function 重载？ 由于目前没有func(index,val1, val2)的需求暂时不做泛化处理
+	//现在主要用于normalize 注意TLC中Vector.ApplyInto 是func<index, val1, val2> 只是val2 传递的时候未用
+	//func(index, vec.Value(index), vec2.Value(index)
+	//sparse vector and binary vec apply
+	template<typename Func, typename _Vector>
+	void apply_sparse(_Vector& vec, const ivec& shiftIndices, Func func)
+	{
+		_Vector result(vec.Length()); //@NOTICE 一定注意要加上长度构造
+		int len = shiftIndices.size();
+		int len2 = vec.Count();
+
+		Float val;
+		int index, index2;
+		int i = 0, j = 0;
+		for (; i < len && j < len2;)
+		{
+			index = shiftIndices[i];
+			index2 = vec.Index(j);
+			if (index == index2)
+			{
+				val = vec.Value(j);
+				func(index, ref(val));
+				result.Add(index, val);
+				i++;
+				j++;
+			}
+			else if (index < index2)
+			{
+				val = 0;
+				func(index, ref(val));
+				result.Add(index, val);
+				i++;
+			}
+			else
+			{
+				val = vec.Value(j);
+				func(index2, ref(val));
+				result.Add(index2, val);
+				j++;
+			}
+		}
+		for (; i < len; i++)
+		{
+			index = shiftIndices[i];
+			val = 0;
+			func(index, ref(val));
+			result.Add(index, val);
+		}
+
+		for (; j < len2; j++)
+		{
+			index2 = vec.Index(j);
+			val = vec.Value(j);
+			func(index2, ref(val));
+			result.Add(index2, val);
+		}
+
+		//vec.Swap(result);
+		vec = move(result);
+	}
+
+	//以vec为准遍历, shiftIndices 辅助定位有用的非0项
+	template<typename Func>
+	void apply(Vector& vec, const ivec& shiftIndices, Func func)
+	{
+		if (vec.IsDense())
+		{
+			for (size_t i = 0; i < vec.values.size(); i++)
+			{
+				vec.values[i] = func(i, vec.values[i]);
+			}
+		}
+		else
+		{
+			apply_sparse(vec, shiftIndices, func);
+		}
+	}
+}  //----end of namespace gezi
+
+#endif  //----end of NUMERIC__VECTOR_VECTOR_UTIL_H_
---
+++ test/test_binfinder.cc
@@ -0,0 +1,56 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_binfinder.cc
+ *
+ *        \author   chenghuige   
+ *
+ *          \date   2014-04-08 23:41:55.935761
+ *  
+ *  \Description:
+ *
+ *  ==============================================================================
+ */
+
+#define private public
+#define protected public
+#include "common_util.h"
+#include "Numeric/BinFinder.h"
+using namespace std;
+using namespace gezi;
+DEFINE_int32(level, 0, "min log level");
+DEFINE_string(i, "", "input");
+DEFINE_string(o, "", "output");
+DEFINE_string(type, "simple", "");
+
+TEST(test_binfinder, func)
+{
+	BinFinder finder;
+	Fvec vec = {1.0, 2.0, 1.0, 1.2, 1.3, 2.4, 3.8, 4.6, 4.9, 7.2, 7.8, 100.0, 330.1, 10.2};
+	Pvec(vec);
+	Fvec result = finder.FindBins(vec, 3);
+	Pvec(result);
+	result = finder.FindBins(vec, 5);
+	Pvec(result);
+	result = finder.FindBins(vec, 1);
+	Pvec(result);
+	{
+		Fvec vec2;
+		vec2.resize(3);
+		Pvec(vec2);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  int s = google::ParseCommandLineFlags(&argc, &argv, false);
+  if (FLAGS_log_dir.empty())
+    FLAGS_logtostderr = true;
+  FLAGS_minloglevel = FLAGS_level;
+  boost::progress_timer timer;
+  
+  return RUN_ALL_TESTS();
+}
