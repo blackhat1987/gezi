4cde4ffd437c22132e83b425e85779b49d1382eb
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-04-10 12:54:17 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@7369 fa64baa9-71d1-4fed-97ae-c15534abce97

--- test/test_featurevector.cc
+++ test/test_featurevector.cc
@@ -145,6 +145,7 @@ TEST(extractor_old, func)
 
 TEST(other, func)
 {
+	{
 	Vector f1, f2;
 	f1.Add(1, 3);
 	f1.Add(3, 4);
@@ -156,6 +157,22 @@ TEST(other, func)
 
 	f1.ScaleBy(3);
 	Pval(f1.str());
+	}
+	{
+	Vector f1(10);
+	Vector f2;
+	f1.Add(1, 3);
+	f1.Add(3, 4);
+	f2.Add(3, 6);
+	f2.Add(4, 7);
+	f1.ToDense();
+	Pval(dot(f1, f2));
+	Pval(f1.str());
+	Pval(f2.str());
+               
+	f1.ScaleBy(3);
+	Pval(f1.str());
+	}
 }
 
 TEST(other2, func)
@@ -166,7 +183,9 @@ TEST(other2, func)
 	f1.Add(3, 4);
 	f2.Add(3, 6);
 	f2.Add(4, 7);
+	Pval(f1.str());
 	f1.ToDense();
+	Pval(f1.str());
 	Pval(dot(f1, f2));
 	Pval(f1.str());
 	Pval(f2.str());
