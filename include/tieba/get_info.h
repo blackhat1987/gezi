02f0504fce71b7f7508ea6de987017bc7806842e
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-06-21 09:44:04 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@8906 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/tieba/get_info.h
+++ include/tieba/get_info.h
@@ -29,19 +29,33 @@ namespace gezi {
 			curl.setTimeout(timeout);
 			return curl.get(url);
 		}
-		//20140223 目前看这个接口比较慢 200ms-600ms
-		inline string get_post_info(uint64 pid)
+
+
+		//获取帖子内容
+		inline string get_post_info(svec pids)
 		{
-			string section = "tieba.post_info";
-			//因为有%所以 fromat失败
-			/*string url = SCONF_(url, "http://service.tieba.baidu.com/service/antiserver?method=antiUrateTrain&req=a:1:{s:7:%22post_id%22;i:%d;}&format=mcpackraw");*/
-			//	url = (format(url) % pid).str();
-			//string url = SCONF_(url, "http://service.tieba.baidu.com/service/antiserver?method=antiUrateTrain&req=a:1:{s:7:%22post_id%22;i:$pid$;}&format=mcpackraw");
-			string url = "http://service.tieba.baidu.com/service/antiserver?method=antiUrateTrain&req=a:1:{s:7:%22post_id%22;i:$pid$;}&format=mcpackraw";
-			Pval(pid);
-			boost::replace_first(url, "$pid$", STR(pid));
+			string pids_ = gezi::join(pids, ",");
+			string url = (format("http://service.tieba.baidu.com/service/antiserver?method=antiGetRscInfo&post_ids=%s&format=mcpackraw")%pids_).str();
+
+			Pval(url);
+
 			return get_info(url);
 		}
+		//获取用户信息
+
+		////20140223 目前看这个接口比较慢 200ms-600ms
+		//inline string get_post_info(uint64 pid)
+		//{
+		//	string section = "tieba.post_info";
+		//	//因为有%所以 fromat失败
+		//	/*string url = SCONF_(url, "http://service.tieba.baidu.com/service/antiserver?method=antiUrateTrain&req=a:1:{s:7:%22post_id%22;i:%d;}&format=mcpackraw");*/
+		//	//	url = (format(url) % pid).str();
+		//	//string url = SCONF_(url, "http://service.tieba.baidu.com/service/antiserver?method=antiUrateTrain&req=a:1:{s:7:%22post_id%22;i:$pid$;}&format=mcpackraw");
+		//	string url = "http://service.tieba.baidu.com/service/antiserver?method=antiUrateTrain&req=a:1:{s:7:%22post_id%22;i:$pid$;}&format=mcpackraw";
+		//	Pval(pid);
+		//	boost::replace_first(url, "$pid$", STR(pid));
+		//	return get_info(url);
+		//}
 
 		//获取用户关注数目 粉丝数目
 		inline string get_user_fans(uint64 uid)
