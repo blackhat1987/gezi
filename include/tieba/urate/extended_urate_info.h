f7614aac3a9af1a51f8084a4424656fff324fdf6
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-09-21 14:05:38 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@10619 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/tieba/urate/extended_urate_info.h
+++ include/tieba/urate/extended_urate_info.h
@@ -38,7 +38,7 @@ namespace gezi {
 				//	UrateExtractor::info() = move(info);
 				//第一次调用info()是move构造函数 会走到这里,如果第二次就会走operator =
 				//VLOG(0) << "move construct";
-				Init();
+				//Init();
 			}
 
 			//ExtendedUrateInfo& operator = (ExtendedUrateInfo&& other)
@@ -53,30 +53,31 @@ namespace gezi {
 			//	return *this;
 			//}
 			//按理说这个也是正确的 但是很奇怪的是多线程下可能有问题。。 那上面的是不是也可能没触发bug呢 @TODO @FIXME, 如果不是向base部分copy不会有问题？ 
-			//还是底下UrateInfo::operator = (other); 这样写有问题呢 感觉没有=完 就Init了？
-			//ExtendedUrateInfo& operator = (UrateInfo&& other)
-			//{
-			//	//VLOG(0) << "move assignment from urateinfo";
-			//	UrateInfo::operator = (other);
-			//	Init();
-			//	return *this;
-			//}
-
+			//感觉没有=完 就Init了？
 			ExtendedUrateInfo& operator = (UrateInfo&& other)
 			{
 				//VLOG(0) << "move assignment from urateinfo";
-				this->postId = other.postId;
-				this->nowPostInfo = other.nowPostInfo;
-				this->postsInfo = other.postsInfo;
-				this->userInfo = other.userInfo;
-				this->userLikeForumInfo = other.userLikeForumInfo;
-				this->userPostNumInfo = other.userPostNumInfo;
-				this->userPostNumInForum = other.userPostNumInForum;
-				this->urlInfoMap = other.urlInfoMap;
-				this->urlsVec = other.urlsVec;
-				Init();
+				UrateInfo::operator = (other);
+				//Init();
 				return *this;
 			}
+
+			//这样也不行。。。确定是多线程问题 就是没初始化好 就Init了
+			//ExtendedUrateInfo& operator = (UrateInfo&& other)
+			//{
+			//	//VLOG(0) << "move assignment from urateinfo";
+			//	this->postId = other.postId;
+			//	this->nowPostInfo = other.nowPostInfo;
+			//	this->postsInfo = other.postsInfo;
+			//	this->userInfo = other.userInfo;
+			//	this->userLikeForumInfo = other.userLikeForumInfo;
+			//	this->userPostNumInfo = other.userPostNumInfo;
+			//	this->userPostNumInForum = other.userPostNumInForum;
+			//	this->urlInfoMap = other.urlInfoMap;
+			//	this->urlsVec = other.urlsVec;
+			//	Init();
+			//	return *this;
+			//}
 
 			//ExtendedUrateInfo& operator = (const ExtendedUrateInfo&) = default;
 
