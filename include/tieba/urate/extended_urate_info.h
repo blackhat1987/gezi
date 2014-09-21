903f4dfeaeb360b89a46f8f689ede2fa0d564227
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-09-21 14:38:39 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@10623 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/tieba/urate/extended_urate_info.h
+++ include/tieba/urate/extended_urate_info.h
@@ -69,8 +69,7 @@ namespace gezi {
 				Init();
 				return *this;
 			}
-
-			//ExtendedUrateInfo& operator = (const ExtendedUrateInfo&) = default;
+			ExtendedUrateInfo& operator = (const ExtendedUrateInfo&) = default;
 
 			void Init()
 			{
