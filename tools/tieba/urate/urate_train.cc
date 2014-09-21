b64f5f8481b56ae9330cdc349475cb6370727210
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-09-21 11:32:13 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@10596 fa64baa9-71d1-4fed-97ae-c15534abce97

--- tools/tieba/urate/urate_train.cc
+++ tools/tieba/urate/urate_train.cc
@@ -37,7 +37,7 @@ inline Features gen_features(uint64 pid)
 {
 	Features fe;
 	UrateInfo info = try_get_info<UrateInfo>(pid, [](uint64 pid) { return get_urate_info(pid); }, FLAGS_history);
-	//UrateExtractor::info();
+	UrateExtractor::info();
 	if (info.IsValid())
 	{
 		VLOG(0) << "Before move";
