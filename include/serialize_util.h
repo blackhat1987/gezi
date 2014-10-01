be38dccbd277cd7d1a5a30459c92436a22d502ea
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-10-01 14:00:40 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@10704 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/serialize_util.h
+++ include/serialize_util.h
@@ -14,15 +14,17 @@
 #ifndef SERIALIZE_UTIL_H_
 #define SERIALIZE_UTIL_H_
 
+#ifdef NO_CEREAL
 #include "seralize_boost.h"
-
-#include "conf_util.h"
 #include <boost/serialization/nvp.hpp>
+#include "conf_util.h"
 #define GEZI_SERIALIZATION_NVP(name)\
 	boost::serialization::make_nvp(gezi::conf_trim(#name).c_str(), name)
 //namespace serialize = serialize_util;  //这个地方会和creal冲突。。。 尽量不用这种namespace和函数名容易混淆冲突？
 //.. / .. / include / cereal / cereal.hpp : 378 : 18 : error : expected primary - expression before '(' token
 //serialize(*self, const_cast<T &>(t));
+#else 
+#endif
 namespace ser = serialize_util;
 
 #endif  //----end of SERIALIZE_UTIL_H_
