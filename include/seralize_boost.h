4d6c3a7d343a1db360c647b9e3e33eae44a6e887
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-10-01 13:47:17 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@10701 fa64baa9-71d1-4fed-97ae-c15534abce97

---
+++ include/seralize_boost.h
@@ -0,0 +1,21 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   seralize_boost.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-10-01 21:46:59.712921
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef SERALIZE_BOOST_H_
+#define SERALIZE_BOOST_H_
+
+namespace gezi {
+
+}  //----end of namespace gezi
+
+#endif  //----end of SERALIZE_BOOST_H_
---
+++ include/seralize_cereal.h
@@ -0,0 +1,21 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   seralize_cereal.h
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-10-01 21:47:08.615737
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef SERALIZE_CEREAL_H_
+#define SERALIZE_CEREAL_H_
+
+namespace gezi {
+
+}  //----end of namespace gezi
+
+#endif  //----end of SERALIZE_CEREAL_H_
