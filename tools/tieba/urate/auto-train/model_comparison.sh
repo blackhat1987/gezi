bb4092d74e056972d79f1a81b607ab9406b8aa82
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-12-17 14:42:35 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@11496 fa64baa9-71d1-4fed-97ae-c15534abce97

--- tools/tieba/urate/auto-train/model_comparison.sh
+++ tools/tieba/urate/auto-train/model_comparison.sh
@@ -3,5 +3,5 @@ sh ./get-evaluate-corpus.sh $d
 #cat tieba_urate.log.$1* | perl -ane 'if($_=~/PID:\((.+?)\).*score\((.+?)\).*title:\((.+?)\).*content:\((.+?)\).*result:\((.+?)\)/) {print "$1\t$2\t$3\t$4\t$5\n";}' > for_evaluate.txt
 cat ./evaluate.corpus/tieba_urate.log.* | perl -ane 'if($_=~/PID:\((.+?)\).*title:\((.+?)\).*content:\((.+?)\).*result:\((.+?)\)/) {print "$1\t$2\t$3\t$4\t$5\n";}' > evaluate.txt
 ./split-thread-reply.py evaluate.txt evaluate.thread.txt evaluate.reply.txt
-sh ./evaluate-thread.sh 
-sh ./evaluate-reply.sh 
+sh ./evaluate-thread.sh $1
+sh ./evaluate-reply.sh $1
