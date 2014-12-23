3aa51e5c65d2c33535fa3566d09916422f4c4740
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2014-12-23 06:22:00 +0000

    modify
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@11581 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/IProcessor.h
+++ include/IProcessor.h
@@ -17,45 +17,47 @@
 #include <iostream>
 namespace gezi {
 
-using namespace std;
+	using namespace std;
 
-template<typename T>
-class IProcessor
-{
-public:
-    virtual T process(const T &) = 0;
-    virtual ~IProcessor(){}
-};
+	template<typename T>
+	class IProcessor
+	{
+	public:
+		virtual T process(const T &) = 0;
+		virtual ~IProcessor(){}
+	};
 
-typedef IProcessor<string> IStringProcessor;
+	typedef IProcessor<string> IStringProcessor;
 
 
-//---------------below are two examples
-class SampleStringProcessor :public IStringProcessor
-{
-public:
-  virtual ~SampleStringProcessor()
-  {
-    std::cout << "SampleString destruct" << endl;
-  }
-  virtual string process(string input) override
-  {
-    return "SampleStringProcessor " + input;
-  }
-};
+	//---------------below are two examples
+	class SampleStringProcessor :public IStringProcessor
+	{
+	public:
+		virtual ~SampleStringProcessor()
+		{
+			std::cout << "SampleString destruct" << endl;
+		}
+		/* virtual string process(string input) override*/
+		virtual string process(string input)
+		{
+			return "SampleStringProcessor " + input;
+		}
+	};
 
-class SampleStringProcessor2 :public IStringProcessor
-{
-public:
-   virtual ~SampleStringProcessor2()
-  {
-    std::cout << "SampleStrings destruct" << endl;
-  }
-  virtual string process(string input) override
-  {
-    return "SampleStringProcessor2 " + input;
-  }
-};
+	class SampleStringProcessor2 :public IStringProcessor
+	{
+	public:
+		virtual ~SampleStringProcessor2()
+		{
+			std::cout << "SampleStrings destruct" << endl;
+		}
+		//virtual string process(string input) override
+		virtual string process(string input)
+		{
+			return "SampleStringProcessor2 " + input;
+		}
+	};
 
 }  //----end of namespace gezi
 
