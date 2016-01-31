da72a498fa92dfa2d976108ea2c5097c639407d5
chenghuige <chenghuige@fa64baa9-71d1-4fed-97ae-c15534abce97>
2016-01-31 09:30:59 +0000

    ISSUE=2935381
    git-svn-id: https://svn.baidu.com/app/search/sep/trunk/anti-spam/gezi@15407 fa64baa9-71d1-4fed-97ae-c15534abce97

--- include/LoadSave.h
+++ include/LoadSave.h
@@ -13,69 +13,70 @@
 
 #ifndef LOAD_SAVE_H_
 #define LOAD_SAVE_H_
-
+#include <string>
+#include <glog/logging.h>
 namespace gezi {
 
 	class LoadSave
 	{
 	public:
-		virtual void Save(string path)
+		virtual void Save(std::string path)
 		{
 			VLOG(0) << Name() << " try save to " << path;
 		}
 
-		virtual bool Load(string path)
+		virtual bool Load(std::string path)
 		{
 			VLOG(0) << Name() << " try load from " << path;
 			return true;
 		}
 
-		virtual void CustomSave(string path)
+		virtual void CustomSave(std::string path)
 		{
 			VLOG(0) << Name() << " try custom save to " << path;
 		}
 
-		virtual bool CustomLoad(string path)
+		virtual bool CustomLoad(std::string path)
 		{
 			VLOG(0) << Name() << " try custom load from " << path;
 			return true;
 		}
 
 		//另外兼容之前 小写开头的load save看做类似 LoadText, SaveText
-		virtual void SaveText(string file)
+		virtual void SaveText(std::string file)
 		{
 			VLOG(0) << Name() << " try save as text to " << file;
 		}
 
-		virtual bool LoadText(string file)
+		virtual bool LoadText(std::string file)
 		{
 			VLOG(0) << Name() << " try load as text from " << file;
 			return true;
 		}
 
-		virtual void SaveXml(string file)
+		virtual void SaveXml(std::string file)
 		{
 			VLOG(0) << Name() << " try save as xml to " << file;
 		}
 
-		virtual bool LoadXml(string file)
+		virtual bool LoadXml(std::string file)
 		{
 			VLOG(0) << Name() << " try load as xml from " << file;
 			return true;
 		}
 
-		virtual void SaveJson(string file)
+		virtual void SaveJson(std::string file)
 		{
 			VLOG(0) << Name() << " try save as json to " << file;
 		}
 
-		virtual bool LoadJson(string file)
+		virtual bool LoadJson(std::string file)
 		{
 			VLOG(0) << Name() << " load as json from " << file;
 			return true;
 		}
 
-		virtual string Name()
+		virtual std::string Name()
 		{
 			return "";
 		}
