57e291209a000ac3aa4a35892dc9e408101fbddb
chenghuige <29109317@qq.com>
2016-09-28 06:46:29 +0800

    m

--- include/nlp/Vocabulary.h
+++ include/nlp/Vocabulary.h
@@ -7,7 +7,7 @@
  *
  *          \date   2015-10-30 13:48:30.800547
  *
- *  \Description:
+ *  \Description: 
  *  ==============================================================================
  */
 
@@ -20,43 +20,170 @@ namespace gezi {
 class Vocabulary : public CountIdentifer
 {
 public:
-#ifndef GCCXML
-	using CountIdentifer::CountIdentifer;
-#else 
-	Vocabulary() 
-	{
-	}
-	Vocabulary(string file)
-	{
-		Load(file);
-	}
-#endif
-	
-	size_t freq(int index) const
-	{
-		return _values[index];
-	}
 
-	//注意 可能 -1 越界
-	size_t freq(string key) const
-	{
-		return _values[id(key)];
-	}
+  Vocabulary(string file, 
+    int num_reserved_ids = 1,
+    string start_word = "<S>",
+    string end_word = "</S>",
+    string unk_word = "<UNK>")
+    :_num_reserved_ids(num_reserved_ids),
+    _start_word(start_word),
+    _end_word(end_word),
+    _unk_word(unk_word)
+  {
+    Load(file);
 
-	size_t count(int index) const
-	{
-		return _values[index];
-	}
+    _unk_id = CountIdentifer::id(unk_word) + _num_reserved_ids;
+    _start_id = CountIdentifer::id(start_word) + _num_reserved_ids;
+    
+    if (_start_id < _num_reserved_ids)
+    {
+      _start_id = _unk_id;
+    }
+    _end_id = CountIdentifer::id(end_word) + _num_reserved_ids;
+    if (_end_id < _num_reserved_ids)
+    {
+      _end_id = _unk_id;
+    }
+  }
 
-	//注意 可能 -1 越界
-	size_t count(string key) const
-	{
-		return _values[id(key)];
-	}
+  size_t size() const
+  {
+    return CountIdentifer::size() + _num_reserved_ids;
+  }
+
+  inline IdType id(string key) const
+  {
+    HashMap::const_iterator it = _hashdict.find(key);
+    if (it == _hashdict.end())
+      return _unk_id;
+    return it->second + _num_reserved_ids;
+  }
+
+  inline IdType id(string key, IdType default_val) const
+  {
+    HashMap::const_iterator it = _hashdict.find(key);
+    if (it == _hashdict.end())
+      return default_val;
+    return it->second + _num_reserved_ids;
+  }
+  
+  size_t freq(IdType index) const
+  {
+    if (index < _num_reserved_ids)
+    {
+      return 0;
+    }
+    return _values[index - _num_reserved_ids];
+  }
+
+  size_t count(IdType index) const
+  {
+    if (index < _num_reserved_ids)
+    {
+      return 0;
+    }
+    return _values[index - _num_reserved_ids];
+  }
+
+  string key(IdType index) const
+  {
+    if (index < _num_reserved_ids)
+    {
+      if (index == 0)
+      {
+        return "<PAD>";
+      }
+      return format("<PAD{}>", index);
+    }
+    return CountIdentifer::key(index - _num_reserved_ids);
+  }
+
+  size_t value(int index) const
+  {
+    return _values[index - _num_reserved_ids];
+  }
+
+  //---------no out range core now, return 0 if not in vocab
+  size_t get_value(string key) const
+  {
+    HashMap::const_iterator it = _hashdict.find(key);
+    if (it == _hashdict.end())
+      return 0;
+    return _values[it->second];
+  }
+
+  size_t value(string key) const
+  {
+    HashMap::const_iterator it = _hashdict.find(key);
+    if (it == _hashdict.end())
+      return 0;
+    return _values[it->second];
+  }
+
+  size_t freq(string key) const
+  {
+    HashMap::const_iterator it = _hashdict.find(key);
+    if (it == _hashdict.end())
+      return 0;
+    return _values[it->second];
+  }
+
+  size_t count(string key) const
+  {
+    HashMap::const_iterator it = _hashdict.find(key);
+    if (it == _hashdict.end())
+      return 0;
+    return _values[it->second];
+  }
+
+  int num_reserved_ids() const
+  {
+    return _num_reserved_ids;
+  }
+
+  ///text start id
+  IdType start_id() const
+  {
+    return _start_id;
+  }
+
+  ///text end id
+  IdType end_id() const
+  {
+    return _end_id;
+  }
+
+  IdType unk_id() const
+  {
+    return _unk_id;
+  }
+
+  string start_word() const
+  {
+    return _start_word;
+  }
+
+  string end_word() const
+  {
+    return _end_word;
+  }
+
+  string unk_word() const
+  {
+    return _unk_word;
+  }
 
 protected:
 private:
+  IdType _start_id = -1;  ///text start id
+  IdType _end_id = -1;    ///text end id
+  IdType _unk_id = -1;    ///text unknown id
+  int _num_reserved_ids = 0; ///rervered ids for 0 pad or other purpuse
 
+  string _start_word;
+  string _end_word;
+  string _unk_word;
 };
 
 }  //----end of namespace gezi
--- include/nlp/WordCounter.h
+++ include/nlp/WordCounter.h
@@ -22,219 +22,219 @@ namespace gezi {
 class WordCounter 
 {
 public:
-	typedef std::pair<string, size_t> WordFreq;
+  typedef std::pair<string, size_t> WordFreq;
 
-	WordCounter(bool addUnknown = true, string unknownMark = "<UNK>", 
-		size_t mostCommon = 0, size_t minCount = 0, 
-		bool saveCountInfo = true)
-		:_addUnknown(addUnknown), _unknownMark(unknownMark),
-		_mostCommon(mostCommon), _minCount(minCount),
-		_saveCountInfo(saveCountInfo)
-	{
+  WordCounter(bool addUnknown = true, string unknownMark = "<UNK>", 
+    size_t mostCommon = 0, size_t minCount = 0, 
+    bool saveCountInfo = true)
+    :_addUnknown(addUnknown), _unknownMark(unknownMark),
+    _mostCommon(mostCommon), _minCount(minCount),
+    _saveCountInfo(saveCountInfo)
+  {
 
-	}
+  }
 
-	void save(string file) const
-	{
-		save_text(file);
-	}
+  void save(string file) const
+  {
+    save_text(file);
+  }
 
-	void save_text(string file) const
-	{
-		ofstream ofs(file);
-		if (_unknownSize && _addUnknown)
-		{
-			ofs << _unknownMark << "\t" << _unknownSize << endl;
-		}
-		size_t vocabSize = 0;
-		for (const auto& item: _ordered)
-		{
-			ofs << item.first << "\t" << item.second << endl;
-			vocabSize++;
-			if (vocabSize == _vocabSize)
-			{
-				break;
-			}
-		}
-	}
+  void save_text(string file) const
+  {
+    ofstream ofs(file);
+    if (_unknownSize && _addUnknown)
+    {
+      ofs << _unknownMark << "\t" << _unknownSize << endl;
+    }
+    size_t vocabSize = 0;
+    for (const auto& item: _ordered)
+    {
+      ofs << item.first << "\t" << item.second << endl;
+      vocabSize++;
+      if (vocabSize == _vocabSize)
+      {
+        break;
+      }
+    }
+  }
 
-	CountIdentifer get_vocabulary()
-	{
-		CountIdentifer identifer;
-		if (_unknownSize && _addUnknown)
-		{
-			identifer.add(_unknownMark, _unknownSize);
-		}
-		size_t vocabSize = 0;
-		for (const auto& item : _ordered)
-		{
-			identifer.add(item.first, item.second);
-			vocabSize++;
-			if (vocabSize == _vocabSize)
-			{
-				break;
-			}
-		}
-		return identifer;
-	}
+  CountIdentifer get_vocabulary()
+  {
+    CountIdentifer identifer;
+    if (_unknownSize && _addUnknown)
+    {
+      identifer.add(_unknownMark, _unknownSize);
+    }
+    size_t vocabSize = 0;
+    for (const auto& item : _ordered)
+    {
+      identifer.add(item.first, item.second);
+      vocabSize++;
+      if (vocabSize == _vocabSize)
+      {
+        break;
+      }
+    }
+    return identifer;
+  }
 
-	void save_vocabulary(string file) const
-	{
-		auto identifer = get_vocabulary();
+  void save_vocabulary(string file) const
+  {
+    auto identifer = get_vocabulary();
 
-		identifer.Save(file);
-	}
+    identifer.Save(file);
+  }
 
-	Identifer get_identifer()
-	{
-		Identifer identifer;
-		if (_unknownSize && _addUnknown)
-		{
-			identifer.add(_unknownMark);
-		}
-		size_t vocabSize = 0;
-		for (const auto& item : _ordered)
-		{
-			identifer.add(item.first);
-			vocabSize++;
-			if (vocabSize == _vocabSize)
-			{
-				break;
-			}
-		}
-		return identifer;
-	}
+  Identifer get_identifer()
+  {
+    Identifer identifer;
+    if (_unknownSize && _addUnknown)
+    {
+      identifer.add(_unknownMark);
+    }
+    size_t vocabSize = 0;
+    for (const auto& item : _ordered)
+    {
+      identifer.add(item.first);
+      vocabSize++;
+      if (vocabSize == _vocabSize)
+      {
+        break;
+      }
+    }
+    return identifer;
+  }
 
-	void save_identifer(string file) const
-	{
-		auto identifer = get_identifer();
+  void save_identifer(string file) const
+  {
+    auto identifer = get_identifer();
 
-		identifer.Save(file);
-	}
+    identifer.Save(file);
+  }
 
-	void save(string binFile, string countFile)
-	{
+  void save(string binFile, string countFile)
+  {
     finish();
 
-		ofstream ofs(countFile);
+    ofstream ofs(countFile);
 
-		if (_saveCountInfo)
-		{
-			CountIdentifer identifer;
-			if (_unknownSize && _addUnknown)
-			{
-				ofs << _unknownMark << "\t" << _unknownSize << endl;
-				identifer.add(_unknownMark, _unknownSize);
-			}
-			size_t vocabSize = 0;
-			for (const auto& item : _ordered)
-			{
-				ofs << item.first << "\t" << item.second << endl;
-				identifer.add(item.first, item.second);
-				vocabSize++;
-				if (vocabSize == _vocabSize)
-				{
-					break;
-				}
-			}
-			identifer.Save(binFile);
-		}
-		else
-		{
-			Identifer identifer;
-			if (_unknownSize && _addUnknown)
-			{
-				ofs << _unknownMark << "\t" << _unknownSize << endl;
-				identifer.add(_unknownMark);
-			}
-			size_t vocabSize = 0;
-			for (const auto& item : _ordered)
-			{
-				ofs << item.first << "\t" << item.second << endl;
-				identifer.add(item.first);
-				vocabSize++;
-				if (vocabSize == _vocabSize)
-				{
-					break;
-				}
-			}
-			identifer.Save(binFile);
-		}
-	}
+    if (_saveCountInfo)
+    {
+      CountIdentifer identifer;
+      if (_unknownSize && _addUnknown)
+      {
+        ofs << _unknownMark << "\t" << _unknownSize << endl;
+        identifer.add(_unknownMark, _unknownSize);
+      }
+      size_t vocabSize = 0;
+      for (const auto& item : _ordered)
+      {
+        ofs << item.first << "\t" << item.second << endl;
+        identifer.add(item.first, item.second);
+        vocabSize++;
+        if (vocabSize == _vocabSize)
+        {
+          break;
+        }
+      }
+      identifer.Save(binFile);
+    }
+    else
+    {
+      Identifer identifer;
+      if (_unknownSize && _addUnknown)
+      {
+        ofs << _unknownMark << "\t" << _unknownSize << endl;
+        identifer.add(_unknownMark);
+      }
+      size_t vocabSize = 0;
+      for (const auto& item : _ordered)
+      {
+        ofs << item.first << "\t" << item.second << endl;
+        identifer.add(item.first);
+        vocabSize++;
+        if (vocabSize == _vocabSize)
+        {
+          break;
+        }
+      }
+      identifer.Save(binFile);
+    }
+  }
 
-	void add(string word) 
-	{
-		_dict[word]++;
-		_corpusSize++;
-	}
+  void add(string word) 
+  {
+    _dict[word]++;
+    _corpusSize++;
+  }
 
-	//----------------must call most_common or min_count or finish after finish add()
+  //----------------must call most_common or min_count or finish after finish add()
 
-	void most_common(size_t topN)
-	{
-		_ordered = topN >= _dict.size() ? gezi::sort_map_desc(_dict) : gezi::partial_sort_map_desc(_dict, topN);
-		_vocabSize = std::min(_ordered.size(), topN);
-		if (_addUnknown)
-		{
-			_unknownSize = _corpusSize - std::accumulate(_ordered.begin(), _ordered.begin() + _vocabSize, (size_t)0, [](size_t accumulator, const WordFreq& x) { return accumulator + x.second; });
-		}
+  void most_common(size_t topN)
+  {
+    _ordered = topN >= _dict.size() ? gezi::sort_map_desc(_dict) : gezi::partial_sort_map_desc(_dict, topN);
+    _vocabSize = std::min(_ordered.size(), topN);
+    if (_addUnknown)
+    {
+      _unknownSize = _corpusSize - std::accumulate(_ordered.begin(), _ordered.begin() + _vocabSize, (size_t)0, [](size_t accumulator, const WordFreq& x) { return accumulator + x.second; });
+    }
     _finished = true;
-	}
+  }
 
-	void min_count(size_t minCount)
-	{
-		size_t totalCount = 0;
-		for (const auto &item : _dict)
-		{
-			if (item.second >= minCount)
-			{
-				_ordered.push_back(item);
-				totalCount += item.second;
-			}
-		}
-		std::sort(_ordered.begin(), _ordered.end(), CmpPairBySecondDesc());
-		_vocabSize = _ordered.size();
-		_unknownSize = _corpusSize - totalCount;
+  void min_count(size_t minCount)
+  {
+    size_t totalCount = 0;
+    for (const auto &item : _dict)
+    {
+      if (item.second >= minCount)
+      {
+        _ordered.push_back(item);
+        totalCount += item.second;
+      }
+    }
+    std::sort(_ordered.begin(), _ordered.end(), CmpPairBySecondDesc());
+    _vocabSize = _ordered.size();
+    _unknownSize = _corpusSize - totalCount;
     _finished = true;
-	}
-
-	void finish()
-	{
+  }
+  
+  void finish()
+  {
     if (!_finished)
     {
-		  if (_mostCommon)
-		  {
-			  most_common(_mostCommon);
-		  }
-		  else if (_minCount)
-		  {
-			  min_count(_minCount);
-	  	}
-		  else
-		  {
-			  _ordered = gezi::sort_map_desc(_dict);
-			  _vocabSize = _ordered.size();
-		  }
-	  }
+      if (_mostCommon)
+      {
+        most_common(_mostCommon);
+      }
+      else if (_minCount)
+      {
+        min_count(_minCount);
+      }
+      else
+      {
+        _ordered = gezi::sort_map_desc(_dict);
+        _vocabSize = _ordered.size();
+      }
+    }
     _finished = true;
   }
 
 protected:
 private:
-	unordered_map<string, size_t> _dict;
-	
-	vector<WordFreq> _ordered;
-	size_t _corpusSize = 0;
-	size_t _unknownSize = 0;
-	size_t _vocabSize = 0;
+  unordered_map<string, size_t> _dict;
+  
+  vector<WordFreq> _ordered;
+  size_t _corpusSize = 0;
+  size_t _unknownSize = 0;
+  size_t _vocabSize = 0;
 
-	bool _addUnknown = true;
-	string _unknownMark = "<UNK>";
+  bool _addUnknown = true;
+  string _unknownMark = "<UNK>";
 
-	size_t _mostCommon;
-	size_t _minCount;
+  size_t _mostCommon;
+  size_t _minCount;
 
-	bool _saveCountInfo;
+  bool _saveCountInfo;
   bool _finished = false;
 };
 
